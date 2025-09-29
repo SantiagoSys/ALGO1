76a472107e49fa09420780b6106f0e3a5d50b705
Santiago Suarez Elsesser <santiago.suarez.elsesser@gmail.com>
2025-09-29 02:34:53 -0300

    Update 2doCuatriPractica5.hs

--- 2doCuatriPractica5.hs
+++ 2doCuatriPractica5.hs
@@ -55,7 +55,91 @@ quitarTodos :: (Eq t) => t -> [t] -> [t]
 quitarTodos _ [] = []
 quitarTodos e (x:xs) | e == x = quitarTodos e xs
                      | otherwise = x : quitarTodos e xs
+                     
+-- 7)
+eliminarRepetidos :: (Eq t) => [t] -> [t]
+eliminarRepetidos [] = []
+eliminarRepetidos [x] = [x]
+eliminarRepetidos (x:xs) | hayRepetidos (x:xs) = x : eliminarRepetidos (quitarTodos x xs)
+                         | otherwise = x : eliminarRepetidos xs
 
+-- 8)
+mismosElementos :: (Eq t) => [t] -> [t] -> Bool
+mismosElementos [] [] = True
+mismosElementos l1 [] | eliminarRepetidos l1 == [] = True
+                      | otherwise = False
+mismosElementos [] l2 | eliminarRepetidos l2 == [] = True
+                      | otherwise = False
+mismosElementos (x:xs) l2 | pertenece x (eliminarRepetidos l2) = mismosElementos xs (quitarTodos x l2)
+                          | otherwise = False
+
+
+-- EJERCICIO 3 --
+-- 1)
+sumatoria :: [Integer] -> Integer
+sumatoria [] = 0
+sumatoria [x] = x
+sumatoria (x:xs) = x + sumatoria xs
+
+-- 2)
+productoria :: [Integer] -> Integer
+productoria [] = 0
+productoria [x] = x
+productoria (x:xs) = x * productoria xs
+
+-- 3)
+maximo :: [Integer] -> Integer
+maximo [x] = x
+maximo (x:xs) | x >= maximo xs = x
+              | otherwise = maximo xs
+
+-- 4)
+sumarN :: Integer -> [Integer] -> [Integer]
+sumarN _ [] = []
+sumarN n (x:xs) = (x+n) : sumarN n xs
+
+-- 5)
+sumarElPrimero :: [Integer] -> [Integer]
+sumarElPrimero [x] = [x]
+sumarElPrimero (x:xs) = sumarN x (x:xs)
+
+-- 6)
+sumarElUltimo :: [Integer] -> [Integer]
+sumarElUltimo [x] = [x]
+sumarElUltimo (x:xs) = sumarN (ultimo (x:xs)) (x:xs)
+
+-- 7)
+pares :: [Integer] -> [Integer]
+pares [] = []
+pares (x:xs) | mod x 2 == 0 = x : pares xs
+             | otherwise = pares xs
+
+-- 8)
+multiplosDeN :: Integer -> [Integer] -> [Integer]
+multiplosDeN _ [] = []
+multiplosDeN n (x:xs) | mod x n == 0 = x : multiplosDeN n xs
+                      | otherwise = multiplosDeN n xs
+
+-- 9)
+ordenar :: [Integer] -> [Integer]
+ordenar [] = []
+ordenar l = ordenar (quitar (maximo l) l) ++ [maximo l]
+
+
+-- EJERCICIO 4 --
+-- a)
+sacarBlancosRepetidos :: [Char] -> [Char]
+sacarBlancosRepetidos [] = []
+sacarBlancosRepetidos [x] = [x]
+sacarBlancosRepetidos (x:y:xs) | x == ' ' && x == y = x : sacarBlancosRepetidos xs
+                               | otherwise = x : sacarBlancosRepetidos (y:xs)
+
+-- b)
+contarPalabras :: [Char] -> Integer
+contarPalabras [] = 0
+contarPalabras (x:xs) = contarBlancos () + 1
+
+contarBlancos :: [Char] -> Integer
 ordenar :: [Int] -> [Int]
 ordenar [] = []
 ordenar l = ordenar (quitar (maximo l) l) ++ [maximo l]
